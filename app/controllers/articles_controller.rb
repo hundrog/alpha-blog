94ca2abff7befd359750ee0e6c2a93433a557d2a
hundrog <reciog.oscar@hotmail.com>
2019-07-01 17:20:29 -0500

    add pagination to views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
   before_action :set_article, only: [:edit, :update, :show, :destroy]
 
   def index
-    @articles = Article.all.order(:id)
+    @articles = Article.paginate(page: params[:page], per_page: 5)
   end
 
   def new
@@ -15,7 +15,7 @@ class ArticlesController < ApplicationController
   def create
     # render plain: params[:article].inspect
     @article = Article.new(article_params)
-    @article.user = User.first
+    @article.user = User.last
     if @article.save
       flash[:success] = "Article was successfully created"
       redirect_to article_path(@article)
