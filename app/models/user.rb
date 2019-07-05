22b5bdff54f7a36f11e4e7be87dcf42db8013b25
hundrog <reciog.oscar@hotmail.com>
2019-07-05 17:04:35 -0500

    add category and article association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,16 @@
-class User < ActiveRecord::Base
-    has_many :articles, dependent: :destroy
-    before_save { self.email = email.downcase }
+class User < ApplicationRecord
+  has_many :articles, dependent: :destroy
+  before_save { self.email = email.downcase }
 
-    validates   :username, presence: true,
-                uniqueness: {case_sensitive: false}, 
-                length: {minimum: 3, maximum:25}
+  validates :username, presence: true,
+                       uniqueness: { case_sensitive: false },
+                       length: { minimum: 3, maximum: 25 }
 
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
-    validates   :email, presence: true,
-                uniqueness: {case_sensitive: false}, 
-                format: {with: VALID_EMAIL_REGEX}
+  validates :email, presence: true,
+                    uniqueness: { case_sensitive: false },
+                    format: { with: VALID_EMAIL_REGEX }
 
-    has_secure_password
+  has_secure_password
 end
